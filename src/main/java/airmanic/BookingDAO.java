f766876dcf866f391f6dd52c0e22e19c5573639f
Desplat <matt-desplat@wanadoo.fr>
2019-01-17 15:55:52 +0100

    Modify constante

--- src/main/java/airmanic/BookingDAO.java
+++ src/main/java/airmanic/BookingDAO.java
@@ -7,6 +7,8 @@ import javax.persistence.TypedQuery;
 
 public class BookingDAO extends DAO<Booking> {
 
+	private final String SELECT = "select b from Booking b ";
+	
 	public void delete(Booking booking) {
 
 		EntityManager em = DatabaseHelper.createEntityManager();
@@ -25,7 +27,7 @@ public class BookingDAO extends DAO<Booking> {
 		EntityManager em = DatabaseHelper.createEntityManager();
 		DatabaseHelper.beginTx(em);
 
-		TypedQuery<Booking> query = em.createQuery("select b " + "from Booking b " + "join b.flight f" + " where f.number=:nbr", Booking.class);
+		TypedQuery<Booking> query = em.createQuery(SELECT + "join b.flight f" + " where f.number=:nbr", Booking.class);
 
 		query.setParameter("nbr", num);
 		List<Booking> bookings = query.getResultList();
@@ -40,7 +42,7 @@ public class BookingDAO extends DAO<Booking> {
 		EntityManager em = DatabaseHelper.createEntityManager();
 		DatabaseHelper.beginTx(em);
 
-		TypedQuery<Booking> query = em.createQuery("select b " + "from Booking b " + " where b.numReservation=:nbr", Booking.class);
+		TypedQuery<Booking> query = em.createQuery(SELECT + " where b.numReservation=:nbr", Booking.class);
 
 		query.setParameter("nbr", num);
 		Booking booking = query.getSingleResult();
@@ -55,7 +57,7 @@ public class BookingDAO extends DAO<Booking> {
 		EntityManager em = DatabaseHelper.createEntityManager();
 		DatabaseHelper.beginTx(em);
 
-		TypedQuery<Booking> query = em.createQuery("select b " + "from Booking b " + " where b.lastname=:nbr" + " order by b.lastname, b.firstname, b.age", Booking.class);
+		TypedQuery<Booking> query = em.createQuery(SELECT + " where b.lastname=:nbr" + " order by b.lastname, b.firstname, b.age", Booking.class);
 
 		query.setParameter("nbr", name);
 		List<Booking> bookings = query.getResultList();
